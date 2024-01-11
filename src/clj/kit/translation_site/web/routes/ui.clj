4e604684509c5c031a1690d369ed76121aae2a39
amjil <amjil@protonmail.com>
2024-01-11 21:26:02 +0800

    translate language selection

--- src/clj/kit/translation_site/web/routes/ui.clj
+++ src/clj/kit/translation_site/web/routes/ui.clj
@@ -8,6 +8,49 @@
    [reitit.ring.middleware.muuntaja :as muuntaja]
    [reitit.ring.middleware.parameters :as parameters]))
 
+;; (def languages ["Mongolian" "English" "Chinese" "Russian" "Japanese"])
+(def languages ["Spanian" "English" "Chinese" "Russian" "Japanese"])
+
+(defn lang-selector-button [id]
+  [:div {:class "relative py-4"}
+   [:button {:type "button"
+             :class "relative w-full cursor-default rounded-full border-none bg-zinc-100 py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm focus:outline-none sm:text-sm sm:leading-6"
+             :aria-haspopup "listbox" :aria-expanded "true" :aria-labelledby "listbox-label"
+             :_ (str "on click toggle .hidden on #lang-selector-" id)}
+    [:span {:class "flex items-center"}
+     [:span {:class "ml-3 block truncate"}
+      "English"]]
+    [:span {:class "pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"}
+     [:svg {:class "h-5 w-5 text-gray-400"
+            :viewBox "0 0 20 20"
+            :fill "currentColor"
+            :aria-hidden "true"}
+      [:path {:fill-rule "evenodd"
+              :d "M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+              :clip-rule "evenodd"}]]]]
+   [:ul {:class "absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm hidden"
+        ;;  :style "display: none"
+         :id (str "lang-selector-" id)
+         :tabindex "-1" :role "listbox" :aria-labelledby "listbox-label" :aria-activedescendant "listbox-option-0"}
+    (map-indexed
+     (fn [idx itm]
+       [:li {:class "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
+             :id (str "listbox-option-" idx) :role "option"
+             :_ "on click "}
+        [:div {:class "flex items-center"}
+         [:span {:class "font-normal ml-3 block truncate"}
+          itm]]
+        (when (zero? idx)
+          [:span {:class "text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4"}
+           [:svg {:class "h-5 w-5"
+                  :viewBox "0 0 20 20"
+                  :fill "currentColor"
+                  :aria-hidden "true"}
+            [:path {:fill-rule "evenodd"
+                    :d "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    :clip-rule "evenodd"}]]])])
+     languages)]])
+
 (defn home [request]
   (page
    [:head
@@ -45,9 +88,23 @@
        [:a {:href "javascript:void(0)"
             :class "flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
         "Sign in"]]]]
-    [:h1 {:class "text-3xl font-bold underline"}
-     "Welcome to Htmx + Kit module"]
-    [:button {:hx-post "/clicked" :hx-swap "outerHTML"} "Click me!"]]))
+    [:div {:class "h-10"}]
+    [:div {:class "flex flex-col max-w-4xl mx-auto rounded-[18px] border"}
+     [:div {:class "border-b px-4"}
+      [:div {:class "w-full max-w-fit flex"}
+       (lang-selector-button "source")
+       [:div {:class "self-center mx-4"
+              :_ "on click call alert('You clicked me!' + @x)"}
+        [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "none" :viewBox= "0 0 24 24" :stroke-width "1.5" :stroke "currentColor" :class "w-6 h-6"}
+         [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"}]]]
+       (lang-selector-button "target")]]
+     [:div {:class "flex h-60 w-full px-4"}
+      [:div {:class "border-r w-1/2"}
+       [:textarea {:placeholder "Type here"
+                   :autocomplete "off"
+                   :class "resize-none border-none outline-none focus:outline-none outline-none border-transparent focus:border-transparent focus:ring-0"}]]
+      [:div {:class "w-1/2"}
+       [:div {:class "resize-none border-none outline-none focus:outline-none outline-none border-transparent focus:border-transparent focus:ring-0"}]]]]]))
 
 (defn clicked [request]
   (ui
