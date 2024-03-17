f0037a0dd67713e1a88426550a8e1a8126817f94
amjil <amjil@protonmail.com>
2024-03-17 10:24:02 +0800

    update segmentation controller

--- src/clj/kit/translation_site/web/controllers/segmentation.clj
+++ src/clj/kit/translation_site/web/controllers/segmentation.clj
@@ -32,29 +32,29 @@
        [:a {:href "javascript:void(0)"
             ":class" "lang == 'en' ? 'bg-gray-800' : 'bg-gray-400'"
             "@click" "lang = 'en'"
-            :class "mx-2 gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
+            :class "rounded-l-lg gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 md:inline-flex"}
         "English"]
        [:a {:href "javascript:void(0)"
             ":class" "lang == 'mo' ? 'bg-gray-800' : 'bg-gray-400'"
             "@click" "lang = 'mo'"
-            :class "mx-2 gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
+            :class "gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 md:inline-flex"}
         "Mongolian"]
        [:a {:href "javascript:void(0)"
             ":class" "lang == 'ch' ? 'bg-gray-800' : 'bg-gray-400'"
             "@click" "lang = 'ch'"
-            :class "mx-2 gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
+            :class "gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 md:inline-flex"}
         "Chinese"]
        [:a {:href "javascript:void(0)"
             ":class" "lang == 'ja' ? 'bg-gray-800' : 'bg-gray-400'"
             "@click" "lang = 'ja'"
-            :class "mx-2 gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
+            :class "gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 md:inline-flex"}
         "Japanese"]
        [:a {:href "javascript:void(0)"
             ":class" "lang == 'cy' ? 'bg-gray-800' : 'bg-gray-400'"
             "@click" "lang = 'cy'"
-            :class "mx-2 gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"}
+            :class "rounded-r-lg gap-x-2 py-2 px-4 text-white text-xs hover:bg-gray-700 active:bg-gray-900 md:inline-flex"}
         "Cryllic"]]
-      [:div {:class "flex flex-col rounded-[18px] border"}
+      [:div {:class "flex flex-col rounded-lg border bg-white"}
        [:div {:class "flex h-56 w-full px-4 border-b overflow-auto"}
         [:textarea {:placeholder "Type here"
                     :autocomplete "off"
@@ -63,10 +63,10 @@
          (:original_txt params)]]
        [:div {:class "flex h-56 w-full px-4 overflow-auto"}
         (if (not-empty text) text "")]]
-      [:div {:class "w-full px-4"}
+      [:div {:class "w-full px-4 py-8"}
        [:span {}
         "Please enter correct text. This tool switches text into sentences according to the following symbols, with. ! ? ; etc."]]
-      [:div {:class "flex flex-col rounded-[18px] border p-4"}
+      [:div {:class "flex flex-col rounded-lg border p-4 bg-white"}
        [:span (str "Divided into " (if (zero? status) num 0) " sentences in total")]
        [:span {} (if (zero? status)
                    ""
